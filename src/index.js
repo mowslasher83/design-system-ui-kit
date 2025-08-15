16d6e6a50890d70cab2498391ffc735fa7a43b9e
mowslasher83 <radeswaled@hotmail.com>
2025-08-14 22:44:57 -0500

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const app = document.getElementById('app');
+
+app.innerHTML = `<h1>Bộ Công Cụ UI Hệ Thống Thiết Kế</h1>`;
+
+console.log('Bắt đầu phát triển giao diện người dùng!');
